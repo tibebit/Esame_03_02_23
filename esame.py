80e0cf4e36abb74994cc410f0b0a767afd6e1022
Fabio Tiberio <45736931+tibebit@users.noreply.github.com>
2023-01-31 14:50:55 +0000

    Initial Commit

---
+++ esame.py
@@ -0,0 +1,161 @@
+class ExamException(Exception):
+  pass
+#Lo scopo di questa classe e'raggruppare le temperature che appartengono alla stessa giornata
+class Day:
+  def __init__(self, temperatures):
+    self.temperatures = temperatures
+  def compute_excursion(self):
+    if len(self.temperatures) == 1:
+      return None
+    self.temperatures.sort()
+    return self.temperatures[-1] - self.temperatures[0]
+
+#lettore generico di CSV files
+class CSVFile:
+  def __init__(self, name=""):
+    self.name = name
+
+  def get_data(self):
+    elements = []
+    try:
+      my_file = open(self.name, 'r')
+    except OSError as e:
+      raise ExamException("Errore: {}".format(e))
+      
+    for line in my_file:
+      values = line.split(',')
+
+      if values[0] != 'epoch':
+        temp = []
+
+        for item in values:
+          temp.append(item.strip())
+
+        elements.append(temp)
+
+    my_file.close()
+    return elements
+
+#lettore specifico per l'esercizio
+class CSVTimeSeriesFile(CSVFile):
+
+  def get_data(self):
+    elements = super().get_data()
+    if not isinstance(elements, list) or len(elements) == 0:
+      raise ExamException('Errore: la lista dati e\'vuota')
+    parsed_values = []
+
+    for element in elements:
+      add_new_row = True
+      new_row = []
+      for i, column in enumerate(element):
+        if i == 0:
+          #leggo la colonna delle date
+          try:
+            new_row.append(int(column))
+          except:
+            #la conversione a intero fallisce
+            try:
+              #provo prima la conversione a float
+              temp = float(column)
+              new_row.append(int(temp))
+            except:
+              #la data non ha un formato accettabile, quindi interrompo il ciclo e segnalo che la riga non verra'aggiunta all'elenco
+              add_new_row = False
+              break
+        else:
+          try:
+            new_row.append(float(column))
+            add_new_row = True
+            #ho trovato il secondo valore della coppia, ne ignoro eventuali altri
+            break
+          except:
+            add_new_row = False
+            #non e'detto che le colonne siano finite, perciò continuo il ciclo
+            continue
+      if add_new_row is True:
+        parsed_values.append(new_row)
+        
+    self.verify_time_series(parsed_values)
+    return parsed_values
+
+  #controlla l'ordine del dataset ed eventuali duplicazioni
+  def verify_time_series(self, time_series):
+    dates = []
+    for row in time_series:
+      dates.append(row[0])
+      
+    self.verify_time_series_order(dates)
+    self.spot_duplicates(dates)
+    
+
+  def verify_time_series_order(self, dates):
+    if dates != sorted(dates):
+      raise ExamException('Errore: il dataset non e\'ordinato!')
+
+  def spot_duplicates(self, dates):
+    previous = None
+    for date in dates:
+      if date != previous:
+        previous = date
+      else:
+        raise ExamException('Errore: il dataset contiene dei duplicati!')
+      
+
+def compute_daily_max_difference(time_series):
+  if not isinstance(time_series, list) or len(time_series) == 0:
+    raise ExamException('Errore: lista dati vuota')
+  days = split_days_in(time_series)
+  differences = []
+  for day in days:
+    differences.append(day.compute_excursion())
+
+  return differences
+
+def split_days_in(time_series):
+    #contiene una lista di giorni
+    splitted_days = []
+    #tiene traccia della giornata corrente
+    current_epoch = None
+    #accumulatore di temperature dello stesso giorno
+    temperatures = []
+  
+    for element in time_series:
+        epoch = element[0]
+        day_start_epoch = epoch - (epoch % 86400)
+        
+        if current_epoch != day_start_epoch:
+          #e'cambiato giorno
+          if current_epoch is not None:
+            #Non devo piu'aggiungere temperature per questo giorno
+            #quindi salvo le temperature della giornata
+            day = Day(temperatures)
+            splitted_days.append(day) 
+            
+          #sovrascrivo l'accumulatore
+          temperatures = element[1:]
+          #reimposto l'epoch corrente
+          current_epoch = day_start_epoch
+        else:
+          #aggiungo nuove temperature alle precedenti
+          temperatures += element[1:]
+          
+    if len(temperatures) != 0:
+      # ci sono temperature nell'accumulatore, aggiungile alla giornata
+      day = Day(temperatures)
+      splitted_days.append(day)
+    
+    return splitted_days
+
+#esecuzione
+    
+# time_series_file = CSVTimeSeriesFile(name='data.csv')
+# time_series = time_series_file.get_data()
+
+# for element in time_series:
+#   print(element)
+
+# lista = compute_daily_max_difference(time_series)
+# print("\nEscursioni Termiche:\n")
+# for element in lista:
+#   print(element)
---
+++ testing.py
@@ -0,0 +1,110 @@
+import unittest
+from esame import *
+
+
+class TestingEsame(unittest.TestCase):
+
+
+#GET_DATA_TESTS------------------------------------------------------------------------- 
+  def test_get_data_reads_all_data_well(self):
+    time_series_file = CSVTimeSeriesFile('testing_dataset.csv')
+    data = time_series_file.get_data()
+    self.assertEqual(16, len(data))
+  
+  def test_raise_exception_on_empty_file(self):
+    time_series_file = CSVTimeSeriesFile('empty_dataset.csv')
+    self.assertRaises(ExamException, time_series_file.get_data)
+    
+  #successo: dataset ordinato e senza duplicati
+  def test_verify_data_success(self):
+    data = [[1551398400, 21.50], [1551402000, 21.40], [1551409200, 21.34],
+            [1551412800, 21.20]]
+  
+    time_series_file = CSVTimeSeriesFile()
+    try:
+      time_series_file.verify_time_series(data)
+    except:
+      self.fail('Il dataset è ordinato, ma è stata sollevata un\'eccezione')
+
+
+#VERIFY_TIME_SERIES_TESTS--------------------------------------------------------------
+  #failure: dataset fuori ordine
+  def test_verify_data_out_of_order(self):
+    data = [[1554073200,25.02], [1551402000, 21.40], [1551409200, 21.34],
+            [1551412800, 21.20], [1551416400, 21.21], [1551420000, 22.40],
+            [1552525200, 22.03], [1552528800, 21.83], [1552532400, 21.60],
+            [1552536000, 21.32], [1554066000,25.41], [1554069600,25.13],
+            [1554073201,25.02]]
+    time_series_file = CSVTimeSeriesFile()
+    try:
+      time_series_file.verify_time_series(data)
+      self.fail('Eccezione non alzata, il dataset è fuori ordine')
+    except:
+      pass
+
+  #failure: dataset con duplicati
+  def test_verify_data_with_duplicates(self):
+    data = [[1551398400, 21.50], [1551398400, 21.40], [1551409200, 21.34],
+            [1551412800, 21.20]]
+  
+    time_series_file = CSVTimeSeriesFile()
+    try:
+      time_series_file.verify_time_series(data)
+      self.fail('Eccezione non alzata, il dataset è fuori ordine')
+    except:
+      pass
+
+  #failure: dataset con duplicati e fuori ordine
+  def test_verify_data_issues_order_and_duplicates(self):
+    data = [[1551398400, 21.50], [1551398400, 21.40], [1551409200, 21.34], [1551412800, 21.20],[1554073200,25.02], [1551402000, 21.40], [1551409200, 21.34],[1551412800, 21.20], [1551416400, 21.21], [1551420000, 22.40], [1552525200, 22.03], [1552528800, 21.83], [1551402000, 21.60]]
+  
+    time_series_file = CSVTimeSeriesFile()
+    try:
+      time_series_file.verify_time_series(data)
+      self.fail('Eccezione non alzata, il dataset è fuori ordine')
+    except:
+      pass
+
+      
+#COMPUTE_DAILY_MAX_DIFFERENCE_TESTS----------------------------------------------------
+  def test_lenght_of_excursions_list_is_one(self):
+    data = [[1551398400, 21.50]]
+    list = compute_daily_max_difference(data)
+    self.assertEqual(1, len(list))
+    data.append([1551398400, 21.50])
+    list = compute_daily_max_difference(data)
+    self.assertEqual(1, len(list))
+
+  def test_lenght_of_excursions_list_is_two(self):
+    data = [[1551398480, 21.50], [1551398400, 21.50], [1661398400, 21.50]]
+    list = compute_daily_max_difference(data)
+    self.assertEqual(2, len(list))
+
+  def test_list_of_excursions_full_of_None(self):
+    data = [[1551398480, 21.50], [1661398400, 21.50]]
+    lista = compute_daily_max_difference(data)
+    self.assertEqual([None, None], lista)
+
+  def test_list_of_excursion_is_correct(self):
+    data = [[1551398480, 21.50], [1551398490, 21.70], [1661398400, 21.50], [1661398460, 23.50]]
+    lista = compute_daily_max_difference(data)
+    self.assertAlmostEqual(2, lista[1])
+
+  def test_list_of_excursion_is_correct_with_multiple_data(self):
+    data = [[1551398480, 21.50], [1551398490, 21.70], [1551398590, 21.50], [1551398690, 25.50]]
+    lista = compute_daily_max_difference(data)
+    self.assertAlmostEqual(4, lista[0])
+
+  def test_compute_daily_max_difference_raises_on_None_input(self):
+    try:
+      compute_daily_max_difference(None)
+      self.fail('Eccezione non sollevata, None in input')
+    except:
+      pass
+      
+  def test_compute_daily_max_difference_raises_on_empty_input(self):
+    try:
+      compute_daily_max_difference(list())
+      self.fail('Eccezione non sollevata, None in input')
+    except:
+      pass
